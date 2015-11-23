d36aee1c02fef583b13cacbc869439b2dd8d6e1b
Anthony VELLA <avella@e3r11p9.42.fr>
2015-11-23 18:18:46 +0100

    ll

---
+++ ft_memcpy.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void    *ft_memcpy(void *dst, const void *src, size_t n)
+{
+  size_t i;
+  unsigned char *s;
+  unsigned char *d;
+  s = (unsigned char *)src;
+  d = (unsigned char *)dst;
+  i = 0;
+  while( i < n)
+    {
+      d[i] = s[i];
+      i++;
+    }
+  return ((void *)d);
+}
